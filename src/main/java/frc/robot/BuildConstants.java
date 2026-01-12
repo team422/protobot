e125b12f53995c503e832de62b3cbfa27a79dcc0
DynamicWhiteHat <aahil.syed.usa@gmail.com>
2026-01-12 17:51:40 -0500

    Added spindexer files

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "ProtoBot";
+  public static final String MAVEN_NAME = "protobot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "198f6928a042cfff7c9cdeddccc41561e11e157f";
-  public static final String GIT_DATE = "2025-12-14 17:00:40 EST";
-  public static final String GIT_BRANCH = "protobot";
-  public static final String BUILD_DATE = "2026-01-08 17:10:42 EST";
-  public static final long BUILD_UNIX_TIME = 1767910242436L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "6c26b9f524f9b04c14f4c7a2baed8cce1c81cbde";
+  public static final String GIT_DATE = "2026-01-08 17:11:21 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-12 17:49:44 EST";
+  public static final long BUILD_UNIX_TIME = 1768258184009L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
