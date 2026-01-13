27a496076048182e327a929da62c9618f2a8c11d
Cube6047 <ray.marathe@gmail.com>
2026-01-12 20:19:19 -0500

    intakeioreplay and sim

--- src/main/java/frc/robot/subsystems/intake/IntakeIOReplay.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOReplay.java
@@ -1,5 +1,13 @@
 package frc.robot.subsystems.intake;
 
-public class IntakeIOReplay {
+public class IntakeIOReplay implements IntakeIO {
+  @Override
+  public void updateInputs(IntakeInputs inputs) {}
 
+  @Override
+  public void setVoltage(double Voltage) {}
+
+  @Override
+  public void setCurrentLimits(double supplyLimit) {}
 }
+
--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -1,5 +1,56 @@
 package frc.robot.subsystems.intake;
 
-public class IntakeIOSim {
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import frc.robot.Constants.IndexerConstants;
 
+public class IntakeIOSim implements IntakeIO {
+  private DCMotorSim m_Sim;
+  //private DCMotorSim m_topSim;
+
+  private double m_Voltage = 0.0;
+  //private double m_topVoltage = 0.0;
+
+  public IntakeIOSim() {
+    var plant =
+        LinearSystemId.createDCMotorSystem(
+            IndexerConstants.kSimGearbox, IndexerConstants.kSimMOI, IndexerConstants.kSimGearing);
+
+    m_Sim = new DCMotorSim(plant, IndexerConstants.kSimGearbox);
+    //m_topSim = new DCMotorSim(plant, IndexerConstants.kSimGearbox);
+  }
+
+  @Override
+  public void updateInputs(IntakeInputs inputs) {
+    m_Sim.setInputVoltage(m_Voltage);
+    m_Sim.update(0.02);
+
+    inputs.Position = m_Sim.getAngularPositionRotations();
+    inputs.VelocityRPS = m_Sim.getAngularVelocityRPM() / 60;
+    inputs.Current = m_Sim.getCurrentDrawAmps();
+    inputs.Voltage = m_Voltage;
+
+    //m_topSim.setInputVoltage(m_topVoltage);
+    //m_topSim.update(0.02);
+
+    //inputs.topPosition = m_topSim.getAngularPositionRotations();
+    //inputs.topVelocityRPS = m_topSim.getAngularVelocityRPM() / 60;
+    //inputs.topCurrent = m_topSim.getCurrentDrawAmps();
+    //inputs.topVoltage = m_topVoltage;
+
+    // these don't matter in sim
+    inputs.StatorCurrent = 0.0;
+    inputs.MotorIsConnected = false;
+  }
+
+  @Override
+  public void setVoltage(double Voltage) {
+    m_Voltage = Voltage;
+    //m_topVoltage = topVoltage;
+  }
+
+  @Override
+  public void setCurrentLimits(double supplyLimit) {
+    // Not needed for simulation
+  }
 }
