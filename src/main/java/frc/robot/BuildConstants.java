c0d35be428cc6149f46c4c6ef7f9d4c23af61534
DynamicWhiteHat <aahil.syed.usa@gmail.com>
2026-01-14 11:24:52 -0500

    Fixing gitignore?

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "protobot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 20;
-  public static final String GIT_SHA = "2d2dd179025faa52989858b3979d3f595be33b4e";
-  public static final String GIT_DATE = "2026-01-13 22:07:25 EST";
+  public static final int GIT_REVISION = 21;
+  public static final String GIT_SHA = "17c85962479e56d0c22fd44eddb143211ead747d";
+  public static final String GIT_DATE = "2026-01-14 10:47:54 EST";
   public static final String GIT_BRANCH = "Spindexer";
-  public static final String BUILD_DATE = "2026-01-14 10:45:54 EST";
-  public static final long BUILD_UNIX_TIME = 1768405554129L;
+  public static final String BUILD_DATE = "2026-01-14 11:13:42 EST";
+  public static final long BUILD_UNIX_TIME = 1768407222333L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
