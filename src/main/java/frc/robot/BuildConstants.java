6690ba21b6b83cbe1567077a8dfe155a1ffc124d
Shrihari101 <shriharikalagi@gmail.com>
2026-01-16 19:19:15 -0500

    works now

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "protobot-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 24;
-  public static final String GIT_SHA = "604f25d66b5be250a44566b0e575b9f9e6abaf1f";
-  public static final String GIT_DATE = "2026-01-15 16:05:24 EST";
+  public static final int GIT_REVISION = 25;
+  public static final String GIT_SHA = "72fd449c14c9ff8f63835cd0ad52787872418aa9";
+  public static final String GIT_DATE = "2026-01-16 19:11:08 EST";
   public static final String GIT_BRANCH = "intake";
-  public static final String BUILD_DATE = "2026-01-15 16:05:26 EST";
-  public static final long BUILD_UNIX_TIME = 1768511126060L;
+  public static final String BUILD_DATE = "2026-01-16 19:11:10 EST";
+  public static final long BUILD_UNIX_TIME = 1768608670252L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
