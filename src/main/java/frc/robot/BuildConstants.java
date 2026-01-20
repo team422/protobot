864b0d27e324da770ef874c396373f0ce1d96e53
team4 <mtwkid1@gmail.com>
2026-01-20 16:38:53 -0500

    Automated commit on 01/20/2026 16:38:53

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,19 +1,17 @@
 package frc.robot;
 
-/**
- * Automatically generated file containing build version information.
- */
+/** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "protobot-1";
+  public static final String MAVEN_NAME = "protobot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 25;
-  public static final String GIT_SHA = "72fd449c14c9ff8f63835cd0ad52787872418aa9";
-  public static final String GIT_DATE = "2026-01-16 19:11:08 EST";
+  public static final int GIT_REVISION = 26;
+  public static final String GIT_SHA = "6690ba21b6b83cbe1567077a8dfe155a1ffc124d";
+  public static final String GIT_DATE = "2026-01-16 19:19:15 EST";
   public static final String GIT_BRANCH = "intake";
-  public static final String BUILD_DATE = "2026-01-16 19:11:10 EST";
-  public static final long BUILD_UNIX_TIME = 1768608670252L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-20 16:38:27 EST";
+  public static final long BUILD_UNIX_TIME = 1768945107360L;
+  public static final int DIRTY = 0;
 
-  private BuildConstants(){}
+  private BuildConstants() {}
 }
