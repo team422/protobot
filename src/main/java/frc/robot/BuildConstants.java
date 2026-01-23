1ad7b710f1e189562fc971b4faa6d74e1f7eab3e
Toomy422 <26mwood@gsgis.k12.va.us>
2026-01-23 18:41:54 -0500

    Deploy fixes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "protobot-26";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "3529b67092b7b8a6ff02c2aa2453de2583742a0c";
-  public static final String GIT_DATE = "2026-01-23 11:16:39 EST";
-  public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-01-23 12:08:38 EST";
-  public static final long BUILD_UNIX_TIME = 1769188118047L;
+  public static final int GIT_REVISION = 33;
+  public static final String GIT_SHA = "e8f5db8bff494fe1f05e7859f41fb6e357ca7d1b";
+  public static final String GIT_DATE = "2026-01-23 12:08:54 EST";
+  public static final String GIT_BRANCH = "intake";
+  public static final String BUILD_DATE = "2026-01-23 18:34:11 EST";
+  public static final long BUILD_UNIX_TIME = 1769211251849L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
