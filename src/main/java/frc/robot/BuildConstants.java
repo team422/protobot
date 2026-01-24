1824d3de998228fe7d402ef0c800896e6c39a425
shoryamalani <shoryamal@gmail.com>
2026-01-24 13:49:55 -0500

    Automated commit on 01/24/2026 13:49:52

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "protobot-26";
+  public static final String MAVEN_NAME = "protobot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 33;
-  public static final String GIT_SHA = "e8f5db8bff494fe1f05e7859f41fb6e357ca7d1b";
-  public static final String GIT_DATE = "2026-01-23 12:08:54 EST";
+  public static final int GIT_REVISION = 34;
+  public static final String GIT_SHA = "1ad7b710f1e189562fc971b4faa6d74e1f7eab3e";
+  public static final String GIT_DATE = "2026-01-23 18:41:54 EST";
   public static final String GIT_BRANCH = "intake";
-  public static final String BUILD_DATE = "2026-01-23 18:34:11 EST";
-  public static final long BUILD_UNIX_TIME = 1769211251849L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-24 13:49:23 EST";
+  public static final long BUILD_UNIX_TIME = 1769280563277L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
